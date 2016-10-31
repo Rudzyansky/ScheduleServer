fdbb6b161015f44697eee05a1b41722faae01876
VladislavSumin <vladislavsumin@mail.ru>
2016-10-31 16:54:22 +0300

    Доделал класс StaticSettings

--- Server/src/ru/falseteam/schedule/server/StaticSettings.java
+++ Server/src/ru/falseteam/schedule/server/StaticSettings.java
@@ -1,9 +1,8 @@
 package ru.falseteam.schedule.server;
 
 
-import com.sun.org.apache.xalan.internal.xslt.Process;
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -16,6 +15,16 @@ public class StaticSettings {
 
     private static final String CONFIG_MAIN = "main.xml";
     private static final String CONFIG_DATABASE = "database.xml";
+
+    // Main
+    private static int port;
+    private static int updatePort;
+
+    // Database
+    private static String url;
+    private static String user;
+    private static String password;
+
 
     public static void main(String[] args) {
         init();
@@ -32,14 +41,50 @@ public class StaticSettings {
         }
 
         File main = new File(CONFIG_FOLDER + separator + CONFIG_MAIN);
+        File database = new File(CONFIG_FOLDER + separator + CONFIG_DATABASE);
         try {
-            if (!main.exists() && main.createNewFile()) {
+            if (!main.exists() && main.createNewFile())
                 createMainConfigFile(main);
-            }
+
+            if (!database.exists() && database.createNewFile())
+                createDatabaseConfigFile(database);
         } catch (IOException e) {
             Console.err("Can not create config file: " + CONFIG_FOLDER);
             Main.stop();
         }
+
+        // Load config
+        try {
+            Properties properties = new Properties();
+
+            properties.loadFromXML(new FileInputStream(main));
+            port = Integer.parseInt(properties.getProperty("port"));
+            updatePort = Integer.parseInt(properties.getProperty("update_port"));
+
+            properties.loadFromXML(new FileInputStream(database));
+            url = properties.getProperty("url");
+            user = properties.getProperty("user");
+            password = properties.getProperty("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Console.print("Config loaded");
+    }
+
+    private static void createDatabaseConfigFile(File file) {
+        Console.print("Initialize config " + CONFIG_DATABASE);
+        Properties properties = new Properties();
+        properties.setProperty("url", "jdbc:mysql://localhost:3306/schedule");
+        properties.setProperty("user", "user");
+        properties.setProperty("password", "password");
+        try {
+            properties.storeToXML(new FileOutputStream(file), "");
+        } catch (IOException e) {
+            Console.err("Can not open file stream from " + file.getPath());
+        }
+        Console.err("WARNING create default database config, please edit "
+                + CONFIG_FOLDER + File.separator + CONFIG_DATABASE + " file.");
     }
 
     private static void createMainConfigFile(File file) {
@@ -52,5 +97,25 @@ public class StaticSettings {
         } catch (IOException e) {
             Console.err("Can not open file stream from " + file.getPath());
         }
+    }
+
+    public static int getPort() {
+        return port;
+    }
+
+    public static int getUpdatePort() {
+        return updatePort;
+    }
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static String getPassword() {
+        return password;
     }
 }
