5904df8a8c8e97aa29278403e3abf9509b53fbb1
VladislavSumin <vladislavsumin@mail.ru>
2016-10-31 16:34:03 +0300

    Добавил папку конфигов в .gitignore. Дописал создание дефолтных конфигов.

--- Server/src/ru/falseteam/schedule/server/StaticSettings.java
+++ Server/src/ru/falseteam/schedule/server/StaticSettings.java
@@ -1,13 +1,21 @@
 package ru.falseteam.schedule.server;
 
 
+import com.sun.org.apache.xalan.internal.xslt.Process;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author Sumin Vladislav
  */
 public class StaticSettings {
     private static final String CONFIG_FOLDER = "config";
+
+    private static final String CONFIG_MAIN = "main.xml";
+    private static final String CONFIG_DATABASE = "database.xml";
 
     public static void main(String[] args) {
         init();
@@ -20,7 +28,29 @@ public class StaticSettings {
         File config = new File(CONFIG_FOLDER);
         if (!config.exists() && !config.mkdir()) {
             Console.err("Can not create config folder.");
-            //Main.stop();
+            Main.stop();
+        }
+
+        File main = new File(CONFIG_FOLDER + separator + CONFIG_MAIN);
+        try {
+            if (!main.exists() && main.createNewFile()) {
+                createMainConfigFile(main);
+            }
+        } catch (IOException e) {
+            Console.err("Can not create config file: " + CONFIG_FOLDER);
+            Main.stop();
+        }
+    }
+
+    private static void createMainConfigFile(File file) {
+        Console.print("Initialize config " + CONFIG_MAIN);
+        Properties properties = new Properties();
+        properties.setProperty("port", "7101");
+        properties.setProperty("update_port", "7102");
+        try {
+            properties.storeToXML(new FileOutputStream(file), "");
+        } catch (IOException e) {
+            Console.err("Can not open file stream from " + file.getPath());
         }
     }
 }
