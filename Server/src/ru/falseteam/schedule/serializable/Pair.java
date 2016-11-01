009f897925aa15431d040a621062a10a12f33755
Prog <prog32bit@ya.ru>
2016-11-01 23:18:01 +0300

    Глабольный рефакторинг и вечерние посиделки

--- Server/src/ru/falseteam/schedule/serializable/Pair.java
+++ Server/src/ru/falseteam/schedule/serializable/Pair.java
@@ -6,10 +6,21 @@ import java.io.Serializable;
  * Created by Prog on 01.11.16.
  */
 public class Pair implements Serializable {
-    public boolean exists = false;
+    public boolean exists;
     public int id;
     public String name;
     public String audience;
     public String teacher;
     public String lastTask;
+
+    private Pair() {
+    }
+
+    public static class Factory {
+        public static Pair getDefault() {
+            Pair pair = new Pair();
+            pair.exists = false;
+            return pair;
+        }
+    }
 }
