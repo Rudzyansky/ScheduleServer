f73ec243977fa1dba3f963697b492e010d342d8d
VladislavSumin <vladislavsumin@mail.ru>
2016-11-07 19:55:53 +0300

    Поправил serializable классы

--- Server/src/ru/falseteam/schedule/serializable/LessonNumber.java
+++ Server/src/ru/falseteam/schedule/serializable/LessonNumber.java
@@ -9,7 +9,28 @@ public class LessonNumber implements Serializable {
     public Time end;
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LessonNumber that = (LessonNumber) o;
+
+        if (id != that.id) return false;
+        if (begin != null ? !begin.equals(that.begin) : that.begin != null) return false;
+        return end != null ? end.equals(that.end) : that.end == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (begin != null ? begin.hashCode() : 0);
+        result = 31 * result + (end != null ? end.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
-        return id + " " + begin + " " + end;
+        return id + ". " + begin + "-" + end;
     }
 }
