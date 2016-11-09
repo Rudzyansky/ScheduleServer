e0610fa7181ad60ff86c4130ae474917cf630f70
VladislavSumin <vladislavsumin@mail.ru>
2016-11-09 17:32:34 +0300

    РЕЛИЗ. ВЕРИСИЯ 2.0b

--- Server/src/ru/falseteam/schedule/server/StaticSettings.java
+++ Server/src/ru/falseteam/schedule/server/StaticSettings.java
@@ -12,6 +12,7 @@ import java.util.Set;
  * @author Sumin Vladislav
  */
 public class StaticSettings {
+    public static final String VERSION = "2.0b";
     public static final String CONFIG_FOLDER = "config";
 
     private static final String CONFIG_MAIN = "main.xml";
