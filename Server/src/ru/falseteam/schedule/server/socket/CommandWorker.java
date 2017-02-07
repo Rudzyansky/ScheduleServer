eca8fa93b1d7672a2834d4cf01133af8f0d04b2f
Prog <prog32bit@ya.ru>
2017-02-08 00:21:21 +0300

    Изменение прав доступа

--- Server/src/ru/falseteam/schedule/server/socket/CommandWorker.java
+++ Server/src/ru/falseteam/schedule/server/socket/CommandWorker.java
@@ -20,8 +20,8 @@ class CommandWorker {
         permissions.put(Groups.admin, new HashMap<>());
         permissions.put(Groups.developer, new HashMap<>());
 
-        addCommand(new Auth(), Groups.guest);
-        addCommand(new GetLessons(), Groups.developer, Groups.admin, Groups.user);
+        addCommand(new Auth(), Groups.developer, Groups.admin, Groups.user, Groups.unconfirmed, Groups.guest);
+        addCommand(new GetLessons(), Groups.developer, Groups.admin, Groups.user, Groups.unconfirmed);
         addCommand(new UpdateLesson(), Groups.developer, Groups.admin);
         addCommand(new DeleteLesson(), Groups.developer, Groups.admin);
         addCommand(new GetUsers(), Groups.developer, Groups.admin, Groups.user);
